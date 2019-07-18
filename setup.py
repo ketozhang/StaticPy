05be1315174bcff91fe75cc0f2a1d8c4118eb7cb
Keto Zhang <keto.zhang@gmail.com>
2019-07-17 17:10:45 -0700

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
 
 setup(
-    name="notebook",
-    url="http://github.com/ketozhang/notebook",
+    name="StaticPy",
+    description="StaticPy a static site generator built with Python Flask supporting Pandoc and Mathjax.",
+    version="0.0.1",
+    url="http://github.com/ketozhang/StaticPy",
     author="Keto Zhang",
     author_email="keto.zhang@gmail.com",
+    packages=['staticpy']
     python_requires='>=3',
+    install_requires=[
+        'flask>=1.0.0',
+        'flask-assets>=0.12',
+        'frozen-flask>=0.15'
+        'pyyaml>=5.1.1',
+        'python-frontmatter>=0.4.5',
+        'pypandoc>=1.4',
+    ]
 )
