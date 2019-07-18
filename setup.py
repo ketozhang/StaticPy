2128dccf5dc7f95e5e3afef4c09974ba8037e540
Keto Zhang <keto.zhang@gmail.com>
2019-07-18 11:28:52 -0700

    Setup.py ready to install

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     url="http://github.com/ketozhang/StaticPy",
     author="Keto Zhang",
     author_email="keto.zhang@gmail.com",
-    packages=['staticpy']
+    packages=['staticpy'],
     python_requires='>=3',
     install_requires=[
         'flask>=1.0.0',
         'flask-assets>=0.12',
-        'frozen-flask>=0.15'
-        'pyyaml>=5.1.1',
+        'frozen-flask>=0.15',
+        'pyyaml>=5.1',
         'python-frontmatter>=0.4.5',
         'pypandoc>=1.4',
     ]
